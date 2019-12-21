de739903f4a8a772a818e442b1e3c38b3d346517
India Rodriguez <carnes.s.ia17@gmail.com>
2019-12-21 11:43:12 -0700

    testing server

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
-app.options('*', cors())
+
 
 app.use('/api/notes', notesRouter)
 
